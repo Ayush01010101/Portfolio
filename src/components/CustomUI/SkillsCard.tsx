2b19cbed50e0280e5807ede9ced78747f5b95a92
Ayush01010101 <ayushawachar0989@gmail.com>
2025-11-18 15:30:03 +0530

    feat: Add skill icons and update project styling
    - Add new SVG icons for skills.
    - Update SkillsCard component to use new icons.
    - Improve project card styling with subtle shadow.

--- src/components/CustomUI/SkillsCard.tsx
+++ src/components/CustomUI/SkillsCard.tsx
@@ -1,4 +1,3 @@
-
 import { useEffect } from "react";
 import type { ReactNode } from "react";
 import { gsap } from "gsap";
@@ -9,15 +8,17 @@ interface SkillsObj {
 }
 
 const Skils: SkillsObj[] = [
-  { imageurl: "https://images.seeklogo.com/logo-png/27/2/react-logo-png_seeklogo-273845.png", text: "React" },
-  { imageurl: "https://cdn.creazilla.com/icons/3254431/tailwindcss-icon-icon-size_512.png", text: "Tailwind" },
-  { imageurl: "https://logodix.com/logo/374972.png", text: "JavaScript" },
-  { imageurl: "https://cdn.worldvectorlogo.com/logos/typescript.svg", text: "TypeScript" },
-  { imageurl: "https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png", text: "Redux" },
-  { imageurl: "https://cdn.worldvectorlogo.com/logos/next-js.svg", text: "Next.js" },
-  { imageurl: "https://cdn.worldvectorlogo.com/logos/gsap-greensock.svg", text: "GSAP" },
-  { imageurl: "https://cdn-icons-png.flaticon.com/512/25/25231.png", text: "GitHub" },
+  {
+    imageurl: "/Html.svg", text: "HTML"
+  },
+  { imageurl: "/Css.svg", text: "CSS" },
+  { imageurl: "/Javascript.svg", text: "Javascript" },
+  { imageurl: "Typescript.svg", text: "Typescript" },
+  { imageurl: "/Supabase.svg", text: "Supabase" },
+  { imageurl: "Github.svg", text: "Github" },
 ];
+
+
 
 const SkilsCard = (): ReactNode => {
 
@@ -27,7 +28,7 @@ const SkilsCard = (): ReactNode => {
       {Skils.map((skill, index) => (
         <div
           key={skill.text}
-          className="skill-box flex flex-col items-center justify-center gap-2 bg-[#1d1d1d] px-4 py-4 rounded-lg shadow-xl hover:scale-105 transition-transform duration-300"
+          className="skill-box w-36 flex flex-col items-center justify-center gap-2 bg-[#1d1d1d] px-4 py-4 rounded-lg shadow-xl hover:scale-105 transition-transform duration-300"
         >
           {/* Image box */}
           <div className="h-12 w-12 flex justify-center items-center">
